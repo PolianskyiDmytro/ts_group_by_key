34ed771c4a26317244ada652ed9963678eb15467
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-08-25 13:22:37 +0300

    add solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,15 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T>(items: T, key: string): GroupsMap<Partial<T>> {
+  return items.reduce((groups: GroupsMap<Partial<T>>, item: object) => {
+    const groupKey = item[key];
+
+    if (!groups[groupKey]) {
+      groups[groupKey] = [];
+    }
+    groups[groupKey].push(item);
+
+    return groups;
+  }, {});
 }
